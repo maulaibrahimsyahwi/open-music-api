7dcc1273e64c6a6bf274555e52d42cbbd7ac62b2
Maula Ibrahim Syahhwi <boimmuhammad8@gmail.com>
2026-02-05 10:20:00 +0700

    first commit

---
+++ src/utils/index.js
@@ -0,0 +1,25 @@
+const mapDBToModelAlbum = ({ id, name, year }) => ({
+  id,
+  name,
+  year,
+});
+
+const mapDBToModelSong = ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  album_id,
+}) => ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  albumId: album_id,
+});
+
+module.exports = { mapDBToModelAlbum, mapDBToModelSong };
